6c46ec706baab0729d73f2abc32762e8d648d355
Gustavo Souza <GustavoSenai500@gmail.com>
2023-04-06 16:57:06 -0300

    iniciando a Terceira tela com JS

--- front-end/t1/js/api.js
+++ front-end/t1/js/api.js
@@ -4,7 +4,7 @@ export const getAlunos = async (sigla) => {
     const url = `https://api-projeto-lion-school.cyclic.app/v1/lion-school/alunos?sigla=${sigla}`
     const response = await fetch(url)
     const data = await response.json()
-
+    
     return data
 }
 
