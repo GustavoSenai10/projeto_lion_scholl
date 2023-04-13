30578e93ffb95fcf0f93f5d3b8fde616f17ca536
Gustavo <Gustavotenorio010@gmail.com>
2023-04-13 00:25:18 -0300

    desenvolvendo js da terceira tela

--- front-end/t3/index.js
+++ front-end/t3/index.js
@@ -2,35 +2,36 @@
 
 import {getAlunoStatus} from "../t1/js/api.js"
 import{getAlunos} from "../t1/js/api.js"
+import {getTeste} from "./teste.js"
 
-const criarCard = function (aluno){
+console.log(getTeste)
 
+const criarCard = (aluno) => {
+  
   const card = document.createElement('div')
   card.classList.add("card")
 
   const foto = document.createElement('img')
   foto.classList.add('aluno')
-  foto.src = `${aluno.foto}`
+  foto.src = `${aluno.image}`
+
+  const nomeAluno = document.createElement('span')
+  nomeAluno.classList.add('nome_aluno')
+  nomeAluno.textContent = aluno.alunos
+  
+  card.append(foto,nomeAluno)
+  return card
+
+  
+
 } 
+const carregarAluno = (aluno) =>{
+  const cardAluno = document.getElementById('estudante')
+   const componentesCard = getTeste.alunos.map(criarCard)
+  
+   cardAluno.replaceChild(componentesCard)
+}
 
+console.log(carregarAluno)
+carregarAluno()
 
-const ctx = document.getElementById('myChart');
-
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
